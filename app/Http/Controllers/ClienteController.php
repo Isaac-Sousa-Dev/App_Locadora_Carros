8b0bd762226b2a2f0d5d48637ade5759a900cb7c
Isaac-Sousa-Dev <isaac.sousa.1202@gmail.com>
2023-04-03 20:31:06 -0300

    API WebService Rest

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -3,19 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cliente;
+use App\Repositories\ClienteRepository;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreClienteRequest;
 use App\Http\Requests\UpdateClienteRequest;
 
 class ClienteController extends Controller
 {
+
+    public function __construct(Cliente $cliente)
+    {
+        $this->cliente = $cliente;
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $clienteRepository = new ClienteRepository($this->cliente);
+
+        if($request->has('filtro')) {
+            $clienteRepository->filtro($request->filtro);
+        }
+
+        if ($request->has('atributos')) {
+            $clienteRepository->selectAtributos($request->atributos);
+        }
+        
+        //$marcas = Marca::all();
+        //$marcas = $this->marca->with('modelos')->get();
+        return response()->json($clienteRepository->getResultado(), 200);
     }
 
 
@@ -25,9 +44,15 @@ class ClienteController extends Controller
      * @param  \App\Http\Requests\StoreClienteRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreClienteRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate($this->cliente->rules());
+
+        $cliente = $this->cliente->create([
+            'nome' => $request->nome
+        ]);
+
+        return response()->json($cliente, 201);
     }
 
     /**
@@ -36,9 +61,13 @@ class ClienteController extends Controller
      * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function show(Cliente $cliente)
+    public function show($id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+        if($cliente === null) {
+            return response()->json(['erro' => 'Recurso pesquisado não existe!'], 404) ; // Json
+        }
+        return response()->json($cliente, 200) ;
     }
 
     /**
@@ -48,9 +77,38 @@ class ClienteController extends Controller
      * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateClienteRequest $request, Cliente $cliente)
+    public function update(Request $request, $id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+
+        if($cliente === null) {
+            return response()->json(['erro' => 'Impossivel realizae a atualização, o recurso solicitado não existe!'], 404) ;
+        }
+
+        if($request->method() === 'PATCH'){
+
+
+            $regrasDinamicas = array();
+
+            
+
+            //percorrendo todas as regras definidas no model
+            foreach($cliente->rules() as $input => $regra) {
+
+                //Coletar apenas as regras aplicáveis aos parâmetros parciais da requisição PATCH
+                if(array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+            $request->validate($regrasDinamicas);
+        } else {
+            $request->validate($cliente->rules());
+        }
+
+        $cliente->fill($request->all());
+        $cliente->save();
+
+        return response()->json($cliente, 200) ;
     }
 
     /**
@@ -59,8 +117,15 @@ class ClienteController extends Controller
      * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Cliente $cliente)
+    public function destroy($id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+
+        if($cliente === null) {
+            return response()->json(['erro' => 'Impossível realizar a exclusão. O recurso solicitado não existe!'], 404) ;
+        }
+        
+        $cliente->delete();
+        return response()->json(['msg' => 'O cliente foi removido com sucesso!'], 200);
     }
 }
